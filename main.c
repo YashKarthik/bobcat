8a94784c3816cde498ae9d2f080ce3228eb3f67d
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2023-08-03 17:44:14 +0530

    Omits decorations for redirection

--- main.c
+++ main.c
@@ -2,8 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_GRAY    "\x1b[90m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
 
 int main (int argc, char *argv[]) {
+  int isTerminal;
+  if (isatty(fileno(stdout))) {
+    isTerminal = 1;
+  } else {
+    isTerminal = 0;
+  }
+
+  struct winsize windowSize;
+  ioctl(STDOUT_FILENO, TIOCGWINSZ, &windowSize);
 
   for (int i = 1; i < argc; i++) {
     if (access(argv[i], F_OK) != 0) {
@@ -11,7 +30,17 @@ int main (int argc, char *argv[]) {
       return 1;
     }
 
-    printf("------------------------------------\nFile: %s\n------------------------------------\n", argv[i]);
+    if (isTerminal) {
+      for (int w = 0; w < windowSize.ws_col; w++) {
+        printf(ANSI_COLOR_GRAY "-");
+      }
+      printf(ANSI_COLOR_RESET "\nFile: %s\n", argv[i]);
+      for (int w = 0; w < windowSize.ws_col; w++) {
+        printf(ANSI_COLOR_GRAY "-");
+      }
+      printf(ANSI_COLOR_RESET "\n");
+    }
+
 
     FILE *f = fopen(argv[i], "r");
 
